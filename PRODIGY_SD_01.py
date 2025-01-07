1bb2863e5ff5ad7c0700381c2ef9f77f56a5efba
aashish <72561113+aashishbagmar@users.noreply.github.com>
2025-01-07 12:48:16 +0530

    Update PRODIGY_SD_01.py

--- PRODIGY_SD_01.py
+++ PRODIGY_SD_01.py
@@ -4,20 +4,20 @@ temp = float(input("Enter temperature value: "))
 Unit = input("Enter the unit (Celcius, Fahrenheit, Kelvin): ")
 
 if Unit.lower() == "celsius":
-    fahrenheit = (temp * 1.8) + 32
-    kelvin = temp + 273.15
+    fahrenheit = round((temp * 1.8) + 32, 2)
+    kelvin = round(temp + 273.15, 2)
     print(f"Temperature in Fahrenheit: {fahrenheit}")
     print(f"Temperature in Fahrenheit: {kelvin}")
 
 elif Unit.lower() == "fahrenheit":
-    Celsius = ((temp - 32))*(5/9)
-    kelvin = (temp - 32) * (5/9) + 273.15
+    Celsius = round(((temp - 32))*(5/9), 2)
+    kelvin = round((temp - 32) * (5/9) + 273.15, 2)
     print(f"Temperature in Celsius: {Celsius}")
     print(f"Temperature in Kelvin: {kelvin}")
 
 elif Unit.lower() == "kelvin":
-    Celsius = temp - 273.15
-    fahrenheit = (temp - 273.15)*(9/5) + 32
+    Celsius = round(temp - 273.15, 2)
+    fahrenheit = round((temp - 273.15)*(9/5) + 32, 2)
     print(f"Temperature in Celsius: {Celsius}")
-    print(f"Temperature in Kelvin: {fahrenheit}")
+    print(f"Temperature in Fahrenheit: {fahrenheit}")
 
